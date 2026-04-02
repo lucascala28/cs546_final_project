b17631ac97a37d30121d882a5bc9c62dc61dba1e
Luca Scala <scala.luca28@gmail.com>
2026-04-02 18:11:08 -0400

    ok forgot to stage helpers.js lol

--- helpers.js
+++ helpers.js
@@ -1,4 +1,7 @@
 import { ObjectId } from "mongodb";
+import { users } from "./config/mongoCollections.js";
+import { getCommentById } from "./data/comment.js";
+import { getTrailById } from "./data/trail.js";
 
 export const checkId = (id) => {
   if (!id) throw new Error("Id not provided");
@@ -8,12 +11,14 @@ export const checkId = (id) => {
   return id.trim();
 };
 
-export const checkCommentObj = (commentObj) => {
+export const checkCommentObj = async (commentObj) => {
   if (!commentObj) throw new Error("No comment object provided");
   if (typeof commentObj !== "object" || Object.keys(commentObj).length === 0)
     throw new Error("Comment object is invalid");
   /*
+  Shape of Comment
   {
+    trailId,
     username,
     content,
     date,
@@ -21,4 +26,112 @@ export const checkCommentObj = (commentObj) => {
     replies
   }
     */
+  for (const [key, value] of Object.entries(commentObj)) {
+    if (value === undefined) throw new Error("Comment object key provided undefined value");
+    switch (key) {
+      case "trailId":
+        try {
+          checkId(value);
+        } catch {
+          throw new Error("Comment trail id not valid");
+        }
+        await getTrailById(value);
+        break;
+
+      case "username":
+        if (typeof value !== 'string' || value.trim().length === 0) throw new Error("Username value not valid");
+        if (!/^[a-zA-Z0-9.\-]{3,20}$/.test(value)) throw new Error("Commenter username must be between 3-20 characters containing only uppercase/lowercase letters, numbers, periods and hyphens");
+        const userCollection = await users();
+        const userMatch = await userCollection.findOne({ username: value.trim() });
+        if (!userMatch) throw new Error("Commenter username does not match any in user database");
+        break;
+
+      case "content":
+        if (typeof value !== 'string' || value.trim().length === 0) throw new Error("Comment content not valid");
+        break;
+
+      case "date":
+        try {
+          checkDate(value);
+        } catch {
+          throw new Error("Comment date invalid");
+        }
+        break;
+
+      case "upvotes":
+        if (typeof value !== 'number') throw new Error("Comment upvotes invalid (must be a number)");
+        break;
+
+      case "replies":
+        await checkReplyArr(value);
+        break;
+
+      default:
+        throw new Error(`Unknown comment field ${key} in comment object`);
+    }
+  }
+
+  return commentObj;
+
 };
+
+export const checkDate = (dateStr) => { }
+
+export const checkReplyArr = async (replyArr) => {
+  /*
+  Shape of Reply
+  {
+    commentId,
+    username,
+    content,
+    date,
+    upvotes,
+  }
+  */
+  if (!Array.isArray(replyArr) || !replyArr.every(item => typeof item === 'object' && item !== null)) throw new Error("Replies value invalid (must be an array of reply objects)");
+
+  for (const reply of replyArr) {
+    for (const [replyKey, replyValue] of Object.entries(reply)) {
+      if (replyValue === undefined) throw new Error("Reply object key provided undefined value");
+      switch (replyKey) {
+        case "commentId":
+          try {
+            checkId(replyValue);
+          } catch {
+            throw new Error("Comment id of reply is invalid");
+          }
+          await getCommentById(replyValue);
+          break;
+
+        case "username":
+          if (typeof replyValue !== 'string' || replyValue.trim().length === 0) throw new Error("Username value not valid");
+          if (!/^[a-zA-Z0-9.\-]{3,20}$/.test(replyValue)) throw new Error("Replier username must be between 3-20 characters containing only uppercase/lowercase letters, numbers, periods and hyphens");
+          const userCollection = await users();
+          const userMatch = await userCollection.findOne({ username: replyValue.trim() });
+          if (!userMatch) throw new Error("Replier username does not match any in user database");
+          break;
+
+        case "content":
+          if (typeof replyValue !== 'string' || replyValue.trim().length === 0) throw new Error("Reply content not valid");
+          break;
+
+        case "date":
+          try {
+            checkDate(replyValue);
+          } catch {
+            throw new Error("Reply date invalid");
+          }
+          break;
+
+        case "upvotes":
+          if (typeof replyValue !== 'number') throw new Error("Reply upvotes invalid (must be a number)");
+          break;
+
+        default:
+          throw new Error(`Unknown reply field ${replyKey} found in reply object`);
+      }
+    }
+  }
+
+  return replyArr;
+}
