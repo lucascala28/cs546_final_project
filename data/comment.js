ccc59f2f441f6f2944addae6c832cd6033e295fc
Luca Scala <scala.luca28@gmail.com>
2026-04-02 18:10:29 -0400

    Add comment insertion functionality and helper funcs to check comment and reply objects

--- data/comment.js
+++ data/comment.js
@@ -2,10 +2,19 @@ import { comments } from "../config/mongoCollections.js";
 import { ObjectId } from "mongodb";
 import { getTrailById } from "./trail.js";
 
-export const addCommentToTrail = async (trailId, commentObj) => {
-  trailId = checkId(trailId);
-  commentObj = checkCommentObj(commentObj);
-  getTrailById(trailId);
+export const createComment = async (commentObj) => {
+  commentObj = await checkCommentObj(commentObj);
 
-  return getTrailById(trailId);
+  const commentCollection = await comments();
+  const insertInfo = await commentCollection.insertOne(commentObj);
+
+  if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+    throw new Error("Failed to add comment to database.");
+  }
+
+  const comment = getCommentById(insertInfo.insertedId.toString());
+
+  return comment;
 };
+
+export const getCommentById = async (id) => { }
